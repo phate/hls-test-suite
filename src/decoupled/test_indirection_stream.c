4e5b90fd167251c5d0b2c7f9196d95231bf52ea8
David Metz <david.c.metz@ntnu.no>
2025-04-03 20:52:17 +0200

    add test_indirection_stream

--- src/decoupled/test_indirection_stream.c
+++ src/decoupled/test_indirection_stream.c
@@ -5,6 +5,18 @@
 
 #define TYPE float
 
+#define LATENCY 100
+
+extern void hls_stream_enq_uint32t(uint32_t channel, uint32_t data);
+extern uint32_t hls_stream_deq_uint32t(uint32_t channel, uint32_t buffer_slots);
+extern void hls_stream_enq_TYPE(uint32_t channel, TYPE data);
+extern TYPE hls_stream_deq_TYPE(uint32_t channel, uint32_t buffer_slots);
+
+enum decoupled_channels{
+    cols_dec_channel,
+    vec_dec_channel
+};
+
 TYPE kernel(
         const TYPE *restrict vec,
         const uint32_t *restrict cols,
@@ -12,7 +24,14 @@ TYPE kernel(
 ) {
     TYPE sum = 0;
     for (uint32_t j = 0; j < ncols; j++) {
-        TYPE Si = vec[cols[j]];
+        hls_stream_enq_uint32t(cols_dec_channel, cols[j]);
+    }
+    for (uint32_t j = 0; j < ncols; j++) {
+        uint32_t i = hls_stream_deq_uint32t(cols_dec_channel, LATENCY);
+        hls_stream_enq_TYPE(vec_dec_channel, vec[i]);
+    }
+    for (uint32_t j = 0; j < ncols; j++) {
+        TYPE Si = hls_stream_deq_TYPE(vec_dec_channel, LATENCY);
         sum += Si;
     }
     return sum;

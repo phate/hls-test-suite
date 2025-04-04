a9f299761baa3166fcef163a80998dd11788e0c5
David Metz <david.c.metz@ntnu.no>
2025-04-04 14:18:14 +0200

    mergesort

---
+++ src/decoupled/binsearch_decouple_rif_while5.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
+#include <math.h>
+#include <malloc.h>
+#include <stdbool.h>
+
+#define TYPE uint32_t
+
+#define LATENCY 128
+#define OUTSTANDING_READS LATENCY
+
+extern void hls_decouple_request_32(uint32_t channel, uint32_t *addr);
+
+extern uint32_t hls_decouple_response_32(uint32_t channel, uint32_t buffer_slots);
+
+extern void hls_stream_enq_uint32t(uint32_t channel, uint32_t data);
+
+extern uint32_t hls_stream_deq_uint32t(uint32_t channel, uint32_t buffer_slots);
+
+enum decoupled_channels {
+    table_channel,
+    sorted_channel,
+    r_stream,
+    l_stream,
+    element_stream,
+    element_stream2,
+    r_stream2,
+    l_stream2,
+    r_sorted_channel,
+    l_sorted_channel,
+    index_stream,
+    index_stream2,
+    result_stream,
+};
+
+#define EXTRA_ITERATIONS 1
+
+void kernel(
+        const TYPE *table,
+        const TYPE *sorted,
+        int32_t *result,
+        uint32_t table_elements,
+        uint32_t sorted_elements
+) {
+
+    for (uint32_t i = 0; i < table_elements; i++) {
+        hls_decouple_request_32(table_channel, &table[i]);
+    }
+    uint32_t rif = 0; // requests in flight
+    uint32_t table_i = 0;
+    for (uint32_t i = 0; i < table_elements;) {
+        uint32_t l, r, m, index;
+        TYPE element;
+        if (rif < OUTSTANDING_READS && table_i < table_elements) {
+            element = hls_decouple_response_32(table_channel, OUTSTANDING_READS);
+            index = table_i++;
+            l = 0;
+            r = sorted_elements - 1;
+            rif++;
+        } else {
+            element = hls_stream_deq_uint32t(element_stream, OUTSTANDING_READS);
+            l = hls_stream_deq_uint32t(l_stream, OUTSTANDING_READS);
+            r = hls_stream_deq_uint32t(r_stream, OUTSTANDING_READS);
+            index = hls_stream_deq_uint32t(index_stream, OUTSTANDING_READS);
+            TYPE tmp = hls_decouple_response_32(sorted_channel, OUTSTANDING_READS);
+            m = (r + l) >> 1;
+            if (tmp > element) {
+                r = m-1;
+            } else {
+                l = m+1;
+            }
+            bool equal = tmp == element;
+            uint32_t res = -1;
+            if(equal){
+                res = m;
+            }
+            if(equal || !(l<=r)){
+                hls_stream_enq_uint32t(index_stream2, index);
+                hls_stream_enq_uint32t(result_stream, res);
+                i++;
+                rif--;
+                continue;
+            }
+        }
+        m = (r + l) >> 1;
+        hls_stream_enq_uint32t(element_stream, element);
+        hls_stream_enq_uint32t(l_stream, l);
+        hls_stream_enq_uint32t(r_stream, r);
+        hls_stream_enq_uint32t(index_stream, index);
+        hls_decouple_request_32(sorted_channel, &sorted[m]);
+    }
+    for (uint32_t i = 0; i < table_elements; i++) {
+        uint32_t index = hls_stream_deq_uint32t(index_stream2, OUTSTANDING_READS);
+        uint32_t res = hls_stream_deq_uint32t(result_stream, OUTSTANDING_READS);
+        result[index] = res;
+    }
+}
+
+void *allocate(size_t size) {
+    return memalign(4096, size + 4096);
+}
+
+int main() {
+    srand(0);
+    uint32_t sorted_elements = 12345;
+    uint32_t table_elements = 1000;
+    TYPE *table = allocate(table_elements * sizeof(TYPE));
+    TYPE *sorted = allocate(sorted_elements * sizeof(TYPE));
+    int32_t *result = allocate(table_elements * sizeof(int32_t));
+    int32_t *expected_result = allocate(table_elements * sizeof(int32_t));
+    for (size_t i = 0; i < sorted_elements; ++i) {
+        // every second number is contained
+        sorted[i] = i * 2;
+    }
+    for (size_t i = 0; i < table_elements; ++i) {
+        TYPE tmp = rand() % (sorted_elements * 2);
+        table[i] = tmp;
+        expected_result[i] = tmp % 2 == 0 ? tmp / 2 : -1;
+    }
+    kernel(table, sorted, result, table_elements, sorted_elements);
+    for (size_t i = 0; i < table_elements; ++i) {
+        assert(result[i] == expected_result[i]);
+    }
+}
---
+++ src/decoupled/mergesort_simple.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
+#include <math.h>
+#include <malloc.h>
+
+#define MIN(a, b) ((a)>(b)?(b):(a))
+void kernel(
+        uint32_t *table,
+        uint32_t *result,
+        uint32_t n
+) {
+    uint32_t width2;
+    for (uint32_t width = 1; width < n; width = width2) {
+        width2 = width << 1;
+        for (uint32_t i_outer = 0; i_outer < n; i_outer+=width2) {
+            uint32_t i_left = i_outer;
+            uint32_t i_right = MIN(i_outer + width, n);
+            uint32_t i_end = MIN(i_outer + width2, n);
+            uint32_t i = i_left;
+            uint32_t j = i_right;
+            for (uint32_t k = i_left; k < i_end; ++k) {
+                if (i < i_right && (j >= i_end || table[i] <= table[j])) {
+                    result[k] = table[i];
+                    i = i + 1;
+                } else {
+                    result[k] = table[j];
+                    j = j + 1;
+                }
+            }
+        }
+        if(width2<n){
+            for (uint32_t i = 0; i < n; ++i) {
+                table[i] = result[i];
+            }
+        }
+    }
+}
+
+int comp(const void *elem1, const void *elem2) {
+    int f = *((uint32_t *) elem1);
+    int s = *((uint32_t *) elem2);
+    if (f > s) return 1;
+    if (f < s) return -1;
+    return 0;
+}
+
+void sort_ref(uint32_t *arr, uint32_t size) {
+    qsort(arr, size, sizeof(uint32_t), comp);
+}
+
+void *allocate(size_t size) {
+    return memalign(4096, size + 4096);
+}
+
+int main() {
+    srand(0);
+    uint32_t sort_elements = 234;
+    uint32_t *table = allocate(sort_elements * sizeof(uint32_t));
+    uint32_t *result = allocate(sort_elements * sizeof(uint32_t));
+    uint32_t *expected_result = allocate(sort_elements * sizeof(uint32_t));
+    for (size_t i = 0; i < sort_elements; ++i) {
+        uint32_t tmp = rand();
+        table[i] = tmp;
+        expected_result[i] = tmp;
+    }
+    sort_ref(expected_result, sort_elements);
+    kernel(table, result, sort_elements);
+    uint32_t prev_element = 0;
+    for (size_t i = 0; i < sort_elements; ++i) {
+        assert(result[i] == expected_result[i]);
+        assert(expected_result[i] >= prev_element);
+        prev_element = expected_result[i];
+    }
+}
